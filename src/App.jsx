02d0cd756281148e55278f949aa0012a6fe5226b
Kevin Mendoza <ksmv233@gmail.com>
2025-08-19 19:56:20 -0600

    frontend terminado

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,70 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [values, setValues] = useState(['', '', '', '', ''])
+  const [results, setResults] = useState({
+    subtotal : '',
+    discountLabel : '',
+    discount : '',
+    total : '',
+  })
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='container mt-5'>
+      <h1>Calculadora de descuentos</h1>
+      <form>
+        <div className='row mb-3'>
+          {values.map((val, index) => (
+            <div className='col-md-2' key={index}>
+              <label className='form-Label'>Producto {index+1}</label>
+              <input 
+              type="text" 
+              className="form-control" 
+              value={val} 
+              placeholder='valor'
+              />
+            </div>
+          ))}
+          <div className='mb-3'>
+            <button type='button' className='btn btn-success'>
+              calcular
+            </button>
+            <button type='button' className='btn btn-danger'>
+              Limpiar
+            </button>
+          </div>
+          <div className='row mt-4'>
+            <div className='col-md-4'>
+              <label className='form-label'>Subtotal</label>
+              <input 
+              type="text"
+              className='form-control'
+              value={results.subtotal}
+              readOnly 
+              />
+            </div>
+            <div className='col-md-4'>
+              <label className='form-label'>{results.discountLabel || 'Descuento'}</label>
+              <input 
+              type="text"
+              className='form-control'
+              value={results.discount} 
+              readOnly
+              />
+            </div>
+            <div className="col-md-4">
+            <label className="form-label">Total a Pagar</label>
+            <input
+              type="text"
+              className="form-control"
+              value={results.total}
+              readOnly
+            />
+          </div>
+          </div>
+        </div>
+      </form>
+    </div>
   )
 }
 
